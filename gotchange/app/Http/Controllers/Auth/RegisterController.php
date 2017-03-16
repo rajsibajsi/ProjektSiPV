d1c5f818262413c08cdad1f3398303634b34070d
rajsibajsi <luka.reisman@student.um.si>
2017-03-16 12:23:21 +0100

    Test

--- gotchange/app/Http/Controllers/Auth/RegisterController.php
+++ gotchange/app/Http/Controllers/Auth/RegisterController.php
@@ -103,7 +103,8 @@ class RegisterController extends Controller
 	    $authUser = $this->findOrCreateUser($user);
         Auth::login($authUser, true);
 
-        return view('home');
+        //return view('home');
+        return Auth::user()->name;
 
         // $user->token;
         /*
--- gotchange/resources/views/welcome.blade.php
+++ gotchange/resources/views/welcome.blade.php
@@ -99,7 +99,6 @@
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <span>Auth::check()</span>
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
