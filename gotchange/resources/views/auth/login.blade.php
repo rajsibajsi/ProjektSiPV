275206a54142fd3fdbc6de98934681cd69e62dcc
rajsibajsi <luka.reisman@student.um.si>
2017-03-16 10:49:00 +0100

    Changed style

--- gotchange/resources/views/auth/login.blade.php
+++ gotchange/resources/views/auth/login.blade.php
@@ -60,15 +60,8 @@
                             </div>
                         </div>
                     </form>
-                    <span id="titleUnderline"></span>
-                    <div id="fb-root"></div>
-                    <script>(function(d, s, id) {
-                      var js, fjs = d.getElementsByTagName(s)[0];
-                      if (d.getElementById(id)) return;
-                      js = d.createElement(s); js.id = id;
-                      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=381217792258113";
-                      fjs.parentNode.insertBefore(js, fjs);
-                    }(document, 'script', 'facebook-jssdk'));</script>
+                    <span id="titleUnderlineLogin"></span>
+                    <a href="{{ url('/facebook') }}"></a>
                 </div>
             </div>
         </div>
