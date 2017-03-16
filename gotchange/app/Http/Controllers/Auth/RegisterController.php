39ec6c1888772ec96702f16e2836e450739d1b05
rajsibajsi <luka.reisman@student.um.si>
2017-03-16 12:15:28 +0100

    rollback

--- gotchange/app/Http/Controllers/Auth/RegisterController.php
+++ gotchange/app/Http/Controllers/Auth/RegisterController.php
@@ -103,7 +103,7 @@ class RegisterController extends Controller
 	    $authUser = $this->findOrCreateUser($user);
         Auth::login($authUser, true);
 
-        return redirect()->view('home');
+        return view('home');
 
         // $user->token;
         /*
