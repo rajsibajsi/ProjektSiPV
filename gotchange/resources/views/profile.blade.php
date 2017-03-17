0195bed5086eabe1ad5eb63fb56b3fce74336655
rajsibajsi <luka.reisman@student.um.si>
2017-03-17 15:24:49 +0100

    Added route for location, restyled profile page

--- gotchange/resources/views/profile.blade.php
+++ gotchange/resources/views/profile.blade.php
@@ -2,20 +2,29 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-sm-3">
-            <div class="row">
-                <h3>{{ Auth::user()->name }}</h3>
-                <h4>{{ Auth::user()->email }}</h4>
-                <h5>Since {{ Auth::user()->created_at->format('d. m. Y') }}</h5>
+    <div class="col-sm-3">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="row">
+                    <h3>{{ Auth::user()->name }}</h3>
+                    <h4>{{ Auth::user()->email }}</h4>
+                    <h5>Since {{ Auth::user()->created_at->format('d. m. Y') }}</h5>
+                </div>
+                <div class="row">
+                    @if (Auth::user()->lat)
+                        <a roll="button" class="btn btn-link" href="#">See Location</a>
+                    @else
+                        <a roll="button" class="btn btn-link" href="#">Add Location</a>
+                    @endif
+                </div>
             </div>
-            <div class="row">
-                @if (Auth::user()->lat)
-                    <a roll="button" class="btn btn-link" href="#">See Location</a>
-                @else
-                    <a roll="button" class="btn btn-link" href="#">Add Location</a>
-                @endif
-            </div>
+        </div>
+    </div>
+    <div class="col-sm-9">
+        <div class="row">
+            <h2>Album</h2>
+        </div>
+        <div class="col-sm-12">
         </div>
     </div>
 </div>
