0a82cee5c08119dde7da51c1d9bd2c5848d3f567
rajsibajsi <luka.reisman@student.um.si>
2017-03-17 15:18:48 +0100

    Added if/else for location

--- gotchange/resources/views/profile.blade.php
+++ gotchange/resources/views/profile.blade.php
@@ -4,9 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-3">
-            <h3>{{ Auth::user()->name }}</h3>
-            <h4>{{ Auth::user()->email }}</h4>
-            <h5>Since {{ Auth::user()->created_at->format('d. m. Y') }}</h5>
+            <div class="row">
+                <h3>{{ Auth::user()->name }}</h3>
+                <h4>{{ Auth::user()->email }}</h4>
+                <h5>Since {{ Auth::user()->created_at->format('d. m. Y') }}</h5>
+            </div>
+            <div class="row">
+                @if (Auth::user()->lat)
+                @else
+                @endif
+            </div>
         </div>
     </div>
 </div>
