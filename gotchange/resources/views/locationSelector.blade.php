4e6a98648c44794c9b0a1f0c553821fd9cd2aab9
rajsibajsi <luka.reisman@student.um.si>
2017-03-19 15:19:54 +0100

    Added console log for testing location

--- gotchange/resources/views/locationSelector.blade.php
+++ gotchange/resources/views/locationSelector.blade.php
@@ -48,15 +48,16 @@
       function geocodeAddress(geocoder, resultsMap) {
         var address = document.getElementById('address').value;
         geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
+            if (status === 'OK') {
+                resultsMap.setCenter(results[0].geometry.location);
+                var marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location
+                });
+                console.log(results[0].geometry.location);
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
         });
       }
     </script>
