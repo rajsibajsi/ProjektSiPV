cb2bca71bebfdaa5fd44f98a5d14abedaa254201
rajsibajsi <luka.reisman@student.um.si>
2017-03-19 15:26:41 +0100

    Testing for lat lng

--- gotchange/resources/views/locationSelector.blade.php
+++ gotchange/resources/views/locationSelector.blade.php
@@ -54,7 +54,7 @@
                     map: resultsMap,
                     position: results[0].geometry.location
                 });
-                console.log(results[0].geometry.location);
+                console.log(results[0].geometry.location.lat() + ", " + results[0].geometry.location.lng());
             } else {
                 alert('Geocode was not successful for the following reason: ' + status);
             }
