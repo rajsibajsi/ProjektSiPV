93199e651fb9a8a4450652e2a5b07467ba6e1df3
rajsibajsi <luka.reisman@student.um.si>
2017-03-28 22:18:12 +0200

    Added distance

--- gotchange/app/Http/Controllers/HomeController.php
+++ gotchange/app/Http/Controllers/HomeController.php
@@ -27,6 +27,24 @@ class HomeController extends Controller
     public function index()
     {
         $users = User::where('id', '!=', Auth::user()->id)->get();
+
+        for ($i=0; $i < count($users) - 1; $i++) { 
+            $users[$i]->distanceToMe = getDistanceBetweenPointsNew(Auth::user()->lat, Auth::user()->lang, $users[$i]->lat, $users[$i]->lang, $unit = 'Km');
+        }
+
+        asort($users);
+
         return view('home', ['users' => $users]);
+    }
+
+    function getDistanceBetweenPointsNew($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'Km') {
+        $theta = $longitude1 - $longitude2;
+        $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
+        $distance = acos($distance);
+        $distance = rad2deg($distance);
+        $distance = $distance * 60 * 1.1515; switch($unit) {
+            case 'Mi': break; case 'Km' : $distance = $distance * 1.609344;
+        }
+        return (round($distance,2));
     }
 }
