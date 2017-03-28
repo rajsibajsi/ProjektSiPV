b5c4e35c28efcef66bc58099cf3ce70512edec7d
rajsibajsi <luka.reisman@student.um.si>
2017-03-28 22:27:50 +0200

    Restructure of fucntion

--- gotchange/app/Http/Controllers/HomeController.php
+++ gotchange/app/Http/Controllers/HomeController.php
@@ -29,22 +29,16 @@ class HomeController extends Controller
         $users = User::where('id', '!=', Auth::user()->id)->get();
 
         for ($i=0; $i < count($users) - 1; $i++) { 
-            $users[$i]->distanceToMe = getDistanceBetweenPointsNew(Auth::user()->lat, Auth::user()->lang, $users[$i]->lat, $users[$i]->lang, $unit = 'Km');
+            $theta = Auth::user()->lang - $users[$i]->lang;
+            $distance = (sin(deg2rad(Auth::user()->lat)) * sin(deg2rad($users[$i]->lat))) + (cos(deg2rad(Auth::user()->lat)) * cos(deg2rad($users[$i]->lat)) * cos(deg2rad($theta)));
+            $distance = acos($distance);
+            $distance = rad2deg($distance);
+            $distance = $distance * 60 * 1.1515;
+            $users[$i]->distanceToMe = $distance * 1.609344;
         }
 
         asort($users);
 
         return view('home', ['users' => $users]);
-    }
-
-    public function getDistanceBetweenPointsNew($latitude1, $longitude1, $latitude2, $longitude2, $unit = 'Km') {
-        $theta = $longitude1 - $longitude2;
-        $distance = (sin(deg2rad($latitude1)) * sin(deg2rad($latitude2))) + (cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * cos(deg2rad($theta)));
-        $distance = acos($distance);
-        $distance = rad2deg($distance);
-        $distance = $distance * 60 * 1.1515; switch($unit) {
-            case 'Mi': break; case 'Km' : $distance = $distance * 1.609344;
-        }
-        return (round($distance,2));
     }
 }
