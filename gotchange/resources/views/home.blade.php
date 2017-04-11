bd5198b57139f1bcce50c38644667498b3366c49
rajsibajsi <luka.reisman@student.um.si>
2017-04-11 10:03:22 +0200

    Added custom distance

--- gotchange/resources/views/home.blade.php
+++ gotchange/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><div class="row"><div class="col-sm-10">Users</div><div id="nearMe" style="text-align:right; cursor: pointer;" class="col-sm-2"><span class="glyphicon glyphicon-map-marker"></span>Near me</div></div></div>
+                <div class="panel-heading"><div class="row"><div class="col-sm-10">Users</div><div id="nearMe" style="text-align:right; cursor: pointer;" class="col-sm-2"><span class="glyphicon glyphicon-map-marker"></span>Near me</div><input placeholder="50" type="text" id="distanceValue"></div></div>
 
                 <div class="panel-body">
                     @if( ! empty($users) )
@@ -26,7 +26,7 @@ $(document).ready( function() {
             distances.push($(this).text());
         });
         for (var i = 0; i < distances.length; i++) {
-            if (distances[i] > 50) {
+            if (distances[i] > $("#distanceValue").val()) {
                 $(".panel-body > a:nth-child(" + (i + 1) + ")").css("display", "none");
             };
         };
