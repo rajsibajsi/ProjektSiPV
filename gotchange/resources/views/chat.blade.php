5c85baefe6af260a1608442c43247b1b9552571a
rajsibajsi <luka.reisman@student.um.si>
2017-04-12 00:09:04 +0200

    Changed route

--- gotchange/resources/views/chat.blade.php
+++ gotchange/resources/views/chat.blade.php
@@ -24,7 +24,7 @@ $(function(){
         $('.chatTitle').text('New Message');
         $('.chatDiv').empty();
 
-        $('.chatDiv').load('chat/newMessage');
+        $('.chatDiv').load('http://gotchange.sytes.net/chat/newMessage');
     });
 });
 </script>
