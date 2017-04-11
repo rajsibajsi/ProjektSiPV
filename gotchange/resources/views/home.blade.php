8338046f8d7f7c4b081c5114b5f36fb16d6c69e5
rajsibajsi <luka.reisman@student.um.si>
2017-04-11 18:22:16 +0200

    Changed style

--- gotchange/resources/views/home.blade.php
+++ gotchange/resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><div class="row"><div class="col-sm-10">Users</div><div style="text-align:left; cursor: pointer;" class="col-sm-2"><span class="glyphicon glyphicon-map-marker"></span><span id="nearMeButton">Near me</span><input style="float: right; max-width: 50px;" value="50" type="text" id="distanceValue"></div></div></div>
+                <div class="panel-heading"><div class="row"><div class="col-sm-10">Users</div><div style="text-align:left; cursor: pointer;" class="col-sm-2"><span style="padding-top: 3px;" class="glyphicon glyphicon-map-marker"></span><span style="padding-right: 6px; padding-top: 3px;" id="nearMeButton">Near me</span><input style="float: right; max-width: 50px;" value="50" type="text" id="distanceValue"></div></div></div>
 
                 <div class="panel-body">
                     @if( ! empty($users) )
