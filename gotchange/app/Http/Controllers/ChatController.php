374aed7d4958cf15b42501dd37cc385c7d1d8cde
rajsibajsi <luka.reisman@student.um.si>
2017-04-11 23:38:59 +0200

    Removed redundand function

--- gotchange/app/Http/Controllers/ChatController.php
+++ gotchange/app/Http/Controllers/ChatController.php
@@ -31,15 +31,6 @@ class ChatController extends Controller
 		return view('chat_sent_items', ['chatMessages' => $chatMessages]);
 	}
 
-	public function inbox()
-	{
-		$chatMessages = DB::table('chat')
-		->where('reciever_name', Auth::user()->name)
-		->get();
-
-		return view('chat_inbox', ['chatMessages' => $chatMessages]);
-	}
-
 	public function newMessage()
 	{
 		return view('chat_new');
