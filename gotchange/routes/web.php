ab823125c9af7a07311df326d6a5ec69edeb725b
rajsibajsi <luka.reisman@student.um.si>
2017-04-11 23:47:04 +0200

    Rerouting

--- gotchange/routes/web.php
+++ gotchange/routes/web.php
@@ -55,7 +55,7 @@ Route::get('chat/inbox', 'ChatController@inbox');
 
 Route::get('chat/newMessage', 'ChatController@newMessage');
 
-Route::post('chat', 'ChatController@sendMessage')->name("chat/sendMessage");
+Route::post('chat', 'ChatController@sendMessage')->name("sendMessage");
 
 //Route::get('chat', 'ChatController@getCoins')->name("chat/getCoinsForChat");
 
