c0bbbcc73549c49a983aa4f6f8d28ba186728705
rajsibajsi <luka.reisman@student.um.si>
2017-04-13 00:05:36 +0200

    Brought back distance

--- gotchange/resources/views/home.blade.php
+++ gotchange/resources/views/home.blade.php
@@ -5,12 +5,12 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><div class="row"><div class="col-sm-10">Users</div><div style="text-align:left; cursor: pointer;" class="col-sm-2"><span style="padding-top: 3px;" class="glyphicon glyphicon-map-marker"></span><span style="padding-right: 6px; padding-top: 3px;" id="nearMeButton">Near me</span><input style="float: right; max-width: 50px;" value="50" type="text" id="distanceValue"></div></div></div>
+                <div class="panel-heading"><div class="row"><div class="col-sm-10">Users</div><div style="text-align:left; cursor: pointer;" class="col-sm-2"><span style="padding-top: 3px;" class="glyphicon glyphicon-map-marker"></span><div style="padding-right: 6px; padding-top: 3px;" id="nearMeButton">Near me</div><input style="float: right; max-width: 50px;" value="50" type="text" id="distanceValue"></div></div></div>
 
                 <div class="panel-body">
                     @if( ! empty($users) )
                         @foreach ($users as $user)
-                            <a style="text-decoration: none;" href="{{ url('profile/' . $user->id) }}"><h4 style="color: black; padding-bottom: 20px; padding-top: 10px; border-bottom: 1px solid #f5f8fa;">{{ $user->name }}</h4></a>
+                            <a style="text-decoration: none;" href="{{ url('profile/' . $user->id) }}"><h4 style="color: black; padding-bottom: 20px; padding-top: 10px; border-bottom: 1px solid #f5f8fa;">{{ $user->name }}</h4><span style="display: hidden;" class="distance">{{ $user->distanceToMe }}</span></a>
                         @endforeach
                     @endif
                 </div>
