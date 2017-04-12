9598380e437c04aa1cac081172a32baed35aef29
rajsibajsi <luka.reisman@student.um.si>
2017-04-12 17:38:02 +0200

    Changed layout

--- gotchange/resources/views/others.blade.php
+++ gotchange/resources/views/others.blade.php
@@ -23,38 +23,40 @@
                 @if ($var === 0)
                     <div class="row">
                 @endif
-                <div class="col-sm-6 col-md-3" style="margin-right: -15px; margin-left: -15px; margin-bottom: -22px;">
-                    <div class="thumbnail">
-                        <div class="coinClick">
-                            <?php $userHasCoin = 'false' ?>
-                            @foreach($users_coins as $users_coin)
-                                @if($users_coin->id_coin === $coin->id)
-                                    <?php $userHasCoin = 'true' ?>
-                                @endif
-                            @endforeach
 
-                            @if($userHasCoin === 'true')
-                                <img src="http://www.coin-database.com{{ $coin->img }}" alt="Failed to load img" style="opacity: 1">
-                            @endif
+                <?php $userHasCoin = 'false' ?>
+                @foreach($users_coins as $users_coin)
+                    @if($users_coin->id_coin === $coin->id)
+                        <?php $userHasCoin = 'true' ?>
+                    @endif
+                @endforeach
+                @if($userHasCoin === 'true')
+                    <div class="col-sm-6 col-md-3" style="margin-right: -15px; margin-left: -15px; margin-bottom: -22px;">
+                        <div class="thumbnail">
+                            <div class="coinClick">
+                                
+                                    <img src="http://www.coin-database.com{{ $coin->img }}" alt="Failed to load img" style="opacity: 1">
+                                
 
-                            <div class="thumbnailheader" style="text-align: center; display: block; text-overflow: ellipsis; word-wrap: break-word; overflow: hidden; height: 3.6em; line-height: 1.8em;">{{ $coin->description }}</div>
-                            <div class="caption">
-                                <p style="text-align: center;">Country: {{ $coin->country }}</p>
-                                <p style="text-align: center;">Year: {{ $coin->year }}</p>
+                                <div class="thumbnailheader" style="text-align: center; display: block; text-overflow: ellipsis; word-wrap: break-word; overflow: hidden; height: 3.6em; line-height: 1.8em;">{{ $coin->description }}</div>
+                                <div class="caption">
+                                    <p style="text-align: center;">Country: {{ $coin->country }}</p>
+                                    <p style="text-align: center;">Year: {{ $coin->year }}</p>
+                                </div>
+                                <p style="text-align: center;">Coins owned:</p>
                             </div>
-                            <p style="text-align: center;">Coins owned:</p>
-                        </div>
-                        <div>
+                            <div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <?php $var += 1 ?>
-                @if ($var === 2) 
-                    <div class="clearfix visible-sm"></div>
-                @endif
-                @if ($var === 4)
-                    </div>
-                    <?php $var = 0 ?>
+                    <?php $var += 1 ?>
+                    @if ($var === 2) 
+                        <div class="clearfix visible-sm"></div>
+                    @endif
+                    @if ($var === 4)
+                        </div>
+                        <?php $var = 0 ?>
+                    @endif
                 @endif
             @endforeach
         </div>
