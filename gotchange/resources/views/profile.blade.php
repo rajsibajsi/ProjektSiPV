cfe3ea5e2eca711a007426382331e8932282597f
rajsibajsi <luka.reisman@student.um.si>
2017-04-12 17:15:10 +0200

    Test

--- gotchange/resources/views/profile.blade.php
+++ gotchange/resources/views/profile.blade.php
@@ -127,7 +127,7 @@ $(function(){ //Ready handler
 <div class="container">
     <div class="col-sm-12 col-md-3">
         <div class="row">
-            <div id="userInfo" class="col-sm-12" style="position: fixed">
+            <div id="userInfo" class="col-sm-12">
                 <div class="row">
                     <h3>{{ Auth::user()->name }}</h3>
                     <h4>{{ Auth::user()->email }}</h4>
@@ -136,7 +136,7 @@ $(function(){ //Ready handler
                 <div class="row">
                     @if (Auth::user()->lat)
                         <a roll="button" class="btn btn-link" href="{{ route('seeLocation') }}">See Location</a>
-                    @else
+                    @else 
                         <a roll="button" class="btn btn-link" href="{{ route('goToLocation') }}">Add Location</a>
                     @endif
                 </div>
