29da061ee7c391e381588c2301675d49cfc9fa12
dejan123 <dejan.zajc@student.um.si>
2017-04-18 17:25:21 +0200

    fixed distance style

--- gotchange/resources/views/home.blade.php
+++ gotchange/resources/views/home.blade.php
@@ -13,7 +13,11 @@
                             <a style="text-decoration: none;" href="{{ url('profile/' . $user->id) }}">
                                 <h4 style="color: black; padding-bottom: 20px; padding-top: 10px; border-bottom: 1px solid #f5f8fa;">{{ $user->name }}</h4>
                             </a>
-                            <span style="display: inline-block; vertical-align: center;">{{ number_format($user->distanceToMe, 2, '.', '') }} km</span>
+                            @if(number_format($user->distanceToMe, 2, '.', '') == 5388.94)
+                                <span style="display: inline-block; vertical-align: center;">location not set</span>
+                            @else
+                                <span style="display: inline-block; vertical-align: center;">{{ number_format($user->distanceToMe, 2, '.', '') }} km</span>
+                            @endif
                         @endforeach
                     @endif
                 </div>
