8e73c78e5475cd1813b887be12a47e4d07c79283
dejan123 <dejan.zajc@student.um.si>
2017-04-18 17:22:23 +0200

    fixed distance style

--- gotchange/resources/views/home.blade.php
+++ gotchange/resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             <a style="text-decoration: none;" href="{{ url('profile/' . $user->id) }}">
                                 <h4 style="color: black; padding-bottom: 20px; padding-top: 10px; border-bottom: 1px solid #f5f8fa;">{{ $user->name }}</h4>
                             </a>
-                            <span style="display: inline-block; vertical-align: center;">{{ number_format($user->distanceToMe, 2, '.', ''); }} km</span>
+                            <span style="display: inline-block; vertical-align: center;">{{ number_format($user->distanceToMe, 2, '.', '') }} km</span>
                         @endforeach
                     @endif
                 </div>
